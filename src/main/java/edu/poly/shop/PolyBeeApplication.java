47bd7e97c3d5aae5748c4b8593a2a6b9bd22fb00
haonnps12415 <haonnps12415@fpt.edu.vn>
2021-07-15 18:45:37 +0700

    first project

---
+++ src/main/java/edu/poly/shop/PolyBeeApplication.java
@@ -0,0 +1,13 @@
+package edu.poly.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyBeeApplication.class, args);
+	}
+
+}
